daab3c8d7dd6c2002657d6c7431c22b8eeaa87ed
Udaya Kumar S <udaydavis144@gmail.com>
2024-07-16 23:50:42 +0530

    Most of styling done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function onTabClick(e) {
+  console.log(e.target);
   let activeTabs = document.querySelectorAll(".active");
 
   // deactivate existing active tab and panel
@@ -14,7 +15,7 @@ function onTabClick(e) {
 
 const element = document.getElementById("nav-tab");
 
-element.addEventListener("click", onTabClick);
+element.addEventListener("click", onTabClick, true);
 
 const getData = async function () {
   // 1. Loading the Data
@@ -26,29 +27,103 @@ const getData = async function () {
     let menData = data.categories[0];
     let womenData = data.categories[1];
     let kidsData = data.categories[2];
-    console.log(menData);
+
+    // 2.Setting up the markup
 
     menData.category_products.forEach((item) => {
-      document.getElementById("men").insertAdjacentHTML(
+      document.getElementById("menGallery").insertAdjacentHTML(
         "afterBegin",
         `<div class="card">
-                    <p class="tag">On offer</p>
-                    <img src="" alt="">
-                    <p>Title</p>
-                    <li>Myntra</li>
-                    <div>
-
-                        <p>Price</p>
-                        <p>Previous Price</p>
-                        <p>Discount</p>
+                    <p class="badge">
+                    <span style="display:grid;place-items:center; margin-top:8px; font-weight:bold;">
+                      ${item.badge_text == null ? "" : item.badge_text}
+                    </span>
+                    </p>
+                    <img src=${item.image} alt="">
+                    <div class="info">
+                    <div class="main">
+                    <p class="title">${item.title}</p>
+                    <li class="vendor">${item.vendor}</li>
                     </div>
-                    <button>Add to cart</button>`
+                    <div class="prices">
+                    <p class="sellPrice">Rs. ${item.price}</p>
+                    <p class="oldPrice">${item.compare_at_price}</p>
+                    <p class="discount">${Math.round(
+                      ((item.compare_at_price - item.price) /
+                        +item.compare_at_price) *
+                        100
+                    )}% Off</p>
+                    </div>
+                    </div>
+                    <button >Add to cart</button>`
+      );
+    });
+    womenData.category_products.forEach((item) => {
+      document.getElementById("womenGallery").insertAdjacentHTML(
+        "afterBegin",
+        `<div class="card">
+                    <p class="badge">
+                    <span style="display:grid;place-items:center; margin-top:8px; font-weight:bold;">
+                      ${item.badge_text == null ? "" : item.badge_text}
+                    </span>
+                    </p>
+                    <img src=${item.image} alt="">
+                    <div class="info">
+                    <div class="main">
+                    <p class="title">${
+                      item.title.split("").length > 10
+                        ? item.title.split("").slice(0, 11).join("") + ".."
+                        : item.title
+                    }</p>
+                    <li class="vendor">${item.vendor}</li>
+                    </div>
+                    <div class="prices">
+                    <p class="sellPrice">Rs. ${item.price}</p>
+                    <p class="oldPrice">${item.compare_at_price}</p>
+                    <p class="discount">${Math.round(
+                      ((item.compare_at_price - item.price) /
+                        item.compare_at_price) *
+                        100
+                    )}% Off</p>
+                    </div>
+                    </div>
+                    <button >Add to cart</button>`
+      );
+    });
+    kidsData.category_products.forEach((item) => {
+      document.getElementById("kidsGallery").insertAdjacentHTML(
+        "afterBegin",
+        `<div class="card">
+                    <p class="badge">
+                    <span style="display:grid;place-items:center; margin-top:8px; font-weight:bold;">
+                      ${item.badge_text == null ? "" : item.badge_text}
+                    </span>
+                    </p>
+                    <img src=${item.image} alt="">
+                    <div class="info">
+                    <div class="main">
+                    <p class="title">${
+                      item.title.split("").length > 10
+                        ? item.title.split("").slice(0, 11).join("") + ".."
+                        : item.title
+                    }</p>
+                    <li class="vendor">${item.vendor}</li>
+                    </div>
+                    <div class="prices">
+                    <p class="sellPrice">Rs. ${item.price}</p>
+                    <p class="oldPrice">${item.compare_at_price}</p>
+                    <p class="discount">${Math.round(
+                      ((item.compare_at_price - item.price) /
+                        item.compare_at_price) *
+                        100
+                    )}% Off</p>
+                    </div>
+                    </div>
+                    <button >Add to cart</button>`
       );
     });
   } catch (err) {
     throw new Error(err.message);
   }
-
-  // 2.Setting up the markup
 };
 getData();
